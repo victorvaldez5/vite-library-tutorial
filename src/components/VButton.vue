a20cf2bae47432cecc229d9c43cbbe70fa61d1ba
Victor V <victorvaldez5@gmail.com>
2021-10-25 16:47:27 -0400

    update to include tailwind

--- src/components/VButton.vue
+++ src/components/VButton.vue
@@ -1,11 +1,5 @@
 <template>
-  <button>
+  <button class="btn" v-bind="$attrs">
     <slot />
   </button>
 </template>
-
-<script>
-  export default {};
-</script>
-
-<style></style>
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+/* eslint-disable import/prefer-default-export */
+
+export { default as VButton } from './VButton.vue';
+
+// Other components you plan to publish go here
